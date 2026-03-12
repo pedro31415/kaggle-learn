6dcebeb56e821543b291318750b1a653e66c4f67
Pedro <pedrogoku80@gmail.com>
2026-03-12 13:57:34 -0300

    all study

---
+++ startProgramming/begin/string.py
@@ -0,0 +1,15 @@
+x = 'Pluto is a planet'
+y = "Pluto is a planet"
+
+print(x == y)
+
+print("Hello \nWorld")
+
+my_name = "pedro henrique araujo cardoso"
+print(my_name.upper())
+print(my_name.capitalize())
+print(my_name.title())
+print(my_name.find("henrique"))
+print(my_name.find("carlos"))
+print(my_name.split())
+
---
+++ startProgramming/begin/study-new.py
@@ -0,0 +1,13 @@
+import math as mt
+import pandas as pd
+
+# print(dir(mt))
+# print()
+# print(dir(pd))
+
+pi_number = mt.pi
+print(pi_number)
+
+print(mt.sin(pi_number/2))
+print(mt.cos(pi_number/2))
+print(mt.tan(pi_number/4))
